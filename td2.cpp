1fac804449e21493e8c991d5825e676ffdbc5bb3
Harry-Heng20 <harry.heng@polymtl.ca>
2024-02-04 19:29:35 -0500

    HH: Fonction ajouterFilm

--- td2.cpp
+++ td2.cpp
@@ -58,10 +58,28 @@ string lireString(istream& fichier)
 #pragma endregion//}
 
 //TODO: Une fonction pour ajouter un Film à une ListeFilms, le film existant déjà; on veut uniquement ajouter le pointeur vers le film existant.  Cette fonction doit doubler la taille du tableau alloué, avec au minimum un élément, dans le cas où la capacité est insuffisante pour ajouter l'élément.  Il faut alors allouer un nouveau tableau plus grand, copier ce qu'il y avait dans l'ancien, et éliminer l'ancien trop petit.  Cette fonction ne doit copier aucun Film ni Acteur, elle doit copier uniquement des pointeurs.
+void ajouterFilm(ListeFilms& liste, Film* film) {
+	if (liste.nElements == liste.capacite) {
+		liste.capacite = max(1, liste.capacite * 2);
+		Film** nouvelleListe = new Film*[liste.capacite];
+		for (int i = 0; i < liste.nElements; i++) {
+			nouvelleListe[i] = liste.elements[i];
+		}
+		delete[] liste.elements;
+		liste.elements = nouvelleListe;
+	}
+	liste.elements[liste.nElements++] = film;
+}
 
 //TODO: Une fonction pour enlever un Film d'une ListeFilms (enlever le pointeur) sans effacer le film; la fonction prenant en paramètre un pointeur vers le film à enlever.  L'ordre des films dans la liste n'a pas à être conservé.
+void enleverFilm() {
+
+}
 
 //TODO: Une fonction pour trouver un Acteur par son nom dans une ListeFilms, qui retourne un pointeur vers l'acteur, ou nullptr si l'acteur n'est pas trouvé.  Devrait utiliser span.
+Acteur* trouverActeur() {
+
+}
 
 //TODO: Compléter les fonctions pour lire le fichier et créer/allouer une ListeFilms.  La ListeFilms devra être passée entre les fonctions, pour vérifier l'existence d'un Acteur avant de l'allouer à nouveau (cherché par nom en utilisant la fonction ci-dessus).
 Acteur* lireActeur(istream& fichier)
